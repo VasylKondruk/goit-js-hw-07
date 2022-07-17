dc7762137832ce1e1e45dd0ab2db2f02b896edcc
Vasyl Kondruk <vkondruk16@gmail.com>
2022-07-17 16:22:55 +0300

    add task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+function createMarkupGallery() {
+  const itemMarkup = galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join("");
+  galleryRef.insertAdjacentHTML("beforeend", itemMarkup);
+}
+createMarkupGallery();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
